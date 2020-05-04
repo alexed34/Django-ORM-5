769c18f84b1295f0cfd2faee1e9ed0eb958012f9
alexed34 <alexed34@gmail.com>
2020-05-04 15:10:29 +0400

    add like

--- property/admin.py
+++ property/admin.py
@@ -4,10 +4,11 @@ from .models import Flat, Complaint
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town','address', 'owner',]
-#     readonly_fields = ['created_at']
-#     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
-#     list_editable= ['new_building']
+    readonly_fields = ['created_at']
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_editable= ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    raw_id_fields = ['liked_by']
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat', 'user')
